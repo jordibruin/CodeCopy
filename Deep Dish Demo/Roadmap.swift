171b09f14ceb58f867ec89a01cd7bc45018bcccb
Jordi Bruin <jordi@goodsnooze.com>
2024-05-06 14:53:06 -0500

    toggle files cleaner code

---
+++ Deep Dish Demo/Roadmap.swift
@@ -0,0 +1,8 @@
+//
+//  Roadmap.swift
+//  Deep Dish Demo
+//
+//  Created by Jordi Bruin on 06/05/2024.
+//
+
+import Foundation
---
+++ Deep Dish Demo/SmallButton.swift
@@ -0,0 +1,18 @@
+//
+//  SmallButton.swift
+//  Deep Dish Demo
+//
+//  Created by Jordi Bruin on 06/05/2024.
+//
+
+import SwiftUI
+
+struct SmallButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmallButton()
+}
