c1c5f31d7f214621e2785160bbe1d0dd0f7ca87d
Jordi Bruin <jordi@goodsnooze.com>
2024-05-06 11:12:18 -0500

    Initial Commit

---
+++ Deep Dish Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deep Dish Demo
+//
+//  Created by Jordi Bruin on 06/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deep Dish Demo/Deep_Dish_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deep_Dish_DemoApp.swift
+//  Deep Dish Demo
+//
+//  Created by Jordi Bruin on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Deep_Dish_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
